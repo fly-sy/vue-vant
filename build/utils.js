d324c4766a7794ec2df74133c95b59e183a05c8e
fly-sy <942093888@qq.com>
2019-01-22 21:09:21 +0800

    设置rem的2中方式

--- build/utils.js
+++ build/utils.js
@@ -23,12 +23,12 @@ exports.cssLoaders = function (options) {
   }
 
   
-  const px2remLoader = {
-    loader: 'px2rem-loader',
-    options: {
-      remUnit: 75      //设计稿宽度/10
-    }
-  }
+  // const px2remLoader = {
+  //   loader: 'px2rem-loader',
+  //   options: {
+  //     remUnit: 75      //设计稿宽度/10
+  //   }
+  // }
 
   const postcssLoader = {
     loader: 'postcss-loader',
@@ -39,7 +39,7 @@ exports.cssLoaders = function (options) {
 
   // generate loader string to be used with extract text plugin
   function generateLoaders(loader, loaderOptions) {
-    const loaders = options.usePostCSS ? [cssLoader, px2remLoader,postcssLoader] : [cssLoader]
+    const loaders = options.usePostCSS ? [cssLoader,postcssLoader] : [cssLoader]
 
     if (loader) {
       loaders.push({
